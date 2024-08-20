7efc1fb925f8316829e0e5ab2ee8c8877ed6de95
nahidulislamshakin <shakinhabib2000@gmail.com>
2024-08-20 15:39:55 +0600

    homepage indexing view checking if the API is work or not

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:provider/provider.dart';
 import 'package:restapi/view_model/homepage_viewmodel.dart';
 
 class HomePage extends StatefulWidget {
@@ -10,19 +11,43 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    final hpvProvider = Provider.of<HomePageViewModel>(context);
+    final height = MediaQuery.of(context).size.height;
+    final width = MediaQuery.of(context).size.width;
     return Scaffold(
       appBar: AppBar(
         title: Text("Rest API"),
       ),
       body: SafeArea(
-        child: Column(
-          children: [
-            Expanded(
-                child: FutureBuilder(
-                  future: getApiData(),
+        child: SingleChildScrollView(
+          child: Container(
+            height: height,
+            width: width,
+            child: Column(
+
+              children: [
+
+                Expanded(
+                  child: FutureBuilder(
+                    future: hpvProvider.getApiData(),
+                    builder: (context, snapshot){
+                      if(!snapshot.hasData){
+                        return Text("Loading");
+                      }
+                      else{
+                        return ListView.builder(
+                          itemCount: hpvProvider.questionList.length,
+                          itemBuilder: (context,index){
+                            return Text("Index : ${index.toString()}",);
+                          },
+                        );
+                      }
+                    },
+                  ),
                 ),
+              ],
             ),
-          ],
+          ),
         )
       ),
     );
